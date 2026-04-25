7ebfd003480fbe85ca1eea1570dc257b66c37ca8
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:42:50 +0400

    2.16 число по ссылке (ref) и на противоположный

---
+++ Practice2.Task16/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Practice2.Task16;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число:");
+        var inputNumber = Console.ReadLine();
+        if (int.TryParse(inputNumber, out var number))
+        {
+            ChangeSign(ref number);
+            Console.WriteLine($"Число с противовположным знаком: {number}");
+        }
+        else
+        {
+            Console.WriteLine("Неверный формат входных данных.");
+        }
+    }
+
+    /// <summary>
+    /// Изменить знак числа на противоположный.
+    /// </summary>
+    /// <param name="number">число</param>
+    static void ChangeSign(ref int number)
+    {
+        if (number > 0)
+        {
+            number = -number;
+        }
+        else
+        {
+            number = number * (-1);
+        }
+    }
+}
