e7de7c2d66bf561f875be65e7588b0af3712323a
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:47:41 +0400

    2.24 enum дней недели.

---
+++ Practice2.Task24/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Practice2.Task24;
+
+class Program
+{
+    /// <summary>
+    /// Перечисление дней недели. 
+    /// </summary>
+    public enum DayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            Console.WriteLine( Enum.GetName(typeof(DayOfWeek), i)); 
+        }
+
+        Console.ReadLine();
+    }
+}
