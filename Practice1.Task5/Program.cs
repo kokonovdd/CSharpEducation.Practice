bf370d8595bff3c7b91703a411a8c9becd0cf642
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:31:20 +0400

    1.5 Площадь круга

---
+++ Practice1.Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice1.Task5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const float PI = 3.14f;
+        float radius = 10f;
+        float square = radius * radius * PI;
+        Console.WriteLine($"Площадь круга радиуса {radius} = {square}");
+    }
+}
