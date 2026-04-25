a1939bc041fa1ff060feba5ba765a1c9cd5c39f1
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:37:41 +0400

    2.10 Создать массив, заполнив рандомом, и вывести

---
+++ Practice2.Task10/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Practice2.Task10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        if (args.Length == 1)
+        {
+            var inputDim = args[0];
+
+            if (int.TryParse(inputDim, out var dim) && dim > 0)
+            {
+                var array = CreateArray(dim);
+                Console.WriteLine("Массив:");
+                PrintArray(array);
+            }
+            else
+            {
+                Console.WriteLine("Неверный формат входных данных.");
+            }
+        }
+        Console.ReadLine();
+    }
+
+    /// <summary>
+    /// Создать массив, заполненных случайными числами.
+    /// </summary>
+    /// <param name="lengthArray">Размер массива.</param>
+    /// <returns>Созданный массив с заданной размерностью.</returns>
+    static int[] CreateArray(int lengthArray)
+    {
+        int[] result = new int[lengthArray];
+        Random rnd = new Random();
+
+        for (int i = 0; i < lengthArray; i++)
+        {
+            result[i] = rnd.Next(1, 100);
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Вывести массив на экран.
+    /// </summary>
+    /// <param name="array">Массив.</param>
+    static void PrintArray(int[] array)
+    {
+        foreach (var item in array)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+}
