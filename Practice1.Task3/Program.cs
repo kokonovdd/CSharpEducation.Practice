470663b143aec5ff4128ae633f1324e9a13fff4a
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:30:05 +0400

    Приветствие по имени с консоли

---
+++ Practice1.Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice1.Task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите имя: ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Привет, {name}");
+        Console.ReadKey();
+    }
+}
