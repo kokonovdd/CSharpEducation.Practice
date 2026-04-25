01e729da7c3e8af2791b4d2960db01cb5f6bf64e
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:30:48 +0400

    1.4 Объединение двух фраз из консоли

---
+++ Practice1.Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Practice1.Task4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите первую фразу: ");
+        string part1 = Console.ReadLine();
+        Console.Write("Введите вторую фразу: ");
+        string part2 = Console.ReadLine();
+        Console.WriteLine(part1 + " " + part2);
+    }
+}
