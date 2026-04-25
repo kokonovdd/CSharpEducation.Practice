610aad5bcbfe0e1f5e3aea2df21c22b5ae438118
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:25:32 +0400

    1.1) Объявите переменную

---
+++ Practice1.Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Practice1.Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age;
+        string name;
+        string company;
+        bool isMarried;
+        float  weight;
+    }
+}
