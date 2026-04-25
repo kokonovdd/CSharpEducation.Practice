8d32e99fafc078c1c5bd9b843748b2a05f7561a8
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:26:12 +0400

    1.2 Объявите и инициализируйте переменную

---
+++ Practice1.Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice1.Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 27;
+        string name = "Denis";
+        float temp = -1.0f;
+        bool isFemale = false;
+    }
+}
