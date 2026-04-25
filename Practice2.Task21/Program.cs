c02bca0ef17c388db6bbff96ad376a5107f452e8
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:45:58 +0400

    2.21 Угадайте число от 1 до 100.

---
+++ Practice2.Task21/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Practice2.Task21;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int number = rnd.Next(1, 100);
+
+        bool flagExit = false;
+        Console.WriteLine("Угадайте загаданное число от 1 до 100. Для выхода из игры введите команду 'exit'");
+
+        while (!flagExit)
+        {
+            var inputValue = Console.ReadLine();
+            if (inputValue == "exit")
+            {
+                flagExit = true;
+            }
+            else
+            {
+                if (int.TryParse(inputValue, out int value))
+                {
+                    if (number == value)
+                    {
+                        Console.WriteLine($"Вы угадали число! Это {value}.");
+                        flagExit = true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Введите число от 1 до 100:");
+                }
+            }
+        }
+        Console.ReadLine();
+    }
+}
