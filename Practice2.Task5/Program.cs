e7f3e213de7be0793a7f4e3d135240c2946dc48d
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:34:17 +0400

    2.5 Сравнение чисел

---
+++ Practice2.Task5/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Practice2.Task5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        {
+            Console.WriteLine("Программа сравнения двух целых чисел.");
+
+            int a;
+            int b;
+
+            Console.Write("Введите 1 число: ");
+            var input = Console.ReadLine();
+            if (!int.TryParse(input,
+                    out a))
+            {
+                Console.WriteLine("Неверный формат входных данных.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.Write("Введите 2 число: ");
+            input = Console.ReadLine();
+            if (!int.TryParse(input,
+                    out b))
+            {
+                Console.WriteLine("Неверный формат входных данных.");
+                Console.ReadLine();
+                return;
+            }
+
+            if (a == b)
+            {
+                Console.WriteLine($"{a} = {b}");
+            }
+            else
+            {
+                if (a > b)
+                {
+                    Console.WriteLine($"{a} > {b}");
+                }
+                else
+                {
+                    Console.WriteLine($"{a} < {b}");
+                }
+            }
+        }
+    }
+}
