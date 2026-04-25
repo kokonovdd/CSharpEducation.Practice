5de0a6d8009f1bfa93801b57a3ae711e4840b91a
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:46:44 +0400

    2.22 Угадай число от 1 до 100 с попытками

---
+++ Practice2.Task22/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Practice2.Task22;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int number = rnd.Next(1, 100);
+        //Количество возможных попыток.
+        const int numberAttempts = 10;
+        //Количество попыток, которое понадобилось пользователю.
+        int numberAttemptsUser = 0;
+
+        bool flagExit = false;
+        Console.WriteLine("Угадайте загаданное число от 1 до 100. Для выхода из игры введите команду 'exit'");
+
+        while (!flagExit)
+        {
+            Console.WriteLine($"Попытка {numberAttemptsUser + 1} из {numberAttempts}.");
+
+            var inputValue = Console.ReadLine();
+            if (inputValue == "exit")
+            {
+                flagExit = true;
+            }
+            else
+            {
+                numberAttemptsUser++;
+                if (int.TryParse(inputValue, out int value))
+                {
+                    if (number == value)
+                    {
+                        Console.WriteLine($"Вы угадали число c {numberAttemptsUser} попытки! Это {value}.");
+                        flagExit = true;
+                        Console.ReadLine();
+                        return;
+                    }
+                    else
+                    {
+                        if (value<number)
+                        {
+                            Console.WriteLine($"Загаданное число > {value}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Загаданное число < {value}");
+                        }
+                    }
+                }
+                if (numberAttemptsUser == numberAttempts)
+                {
+                    Console.WriteLine($"Вы проиграли. Истратили {numberAttempts} попыток.");
+                    flagExit = true;
+                }
+            }
+        }
+        Console.ReadLine();
+    }
+}
