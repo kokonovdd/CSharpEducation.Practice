ef528caf47ba7e4095bba92fdc4001a4a3addf0f
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:38:49 +0400

    2.11 Создать массив и заполнить определенным числом

---
+++ Practice2.Task11/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Practice2.Task11;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        if (args.Length == 2)
+        {
+            var inputDim = args[0];
+            var inputNumber = args[1];
+
+            if (int.TryParse(inputDim, out var dim) && dim > 0 && int.TryParse(inputNumber, out var number))
+            {
+                var array = CreateArray(dim, number);
+                Console.WriteLine("Массив:");
+                PrintArray(array);
+            }
+            else
+            {
+                Console.WriteLine("Неверный формат входных данных.");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Создать массив, заполненных случайными числами.
+    /// </summary>
+    /// <param name="lengthArray">Размер массива.</param>
+    /// <param name="number">Число, которым заполняется массив.</param>
+    /// <returns>Созданный массив с заданной размерностью.</returns>
+    static int[] CreateArray(int lengthArray, int number)
+    {
+        int[] result = new int[lengthArray];
+
+        for (int i = 0; i < lengthArray; i++)
+        {
+            result[i] = number;
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Вывести массив на экран.
+    /// </summary>
+    /// <param name="array">Массив.</param>
+    static void PrintArray(int[] array)
+    {
+        foreach (var item in array)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
