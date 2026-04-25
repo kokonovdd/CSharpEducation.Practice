a3b5c898771d82c4ccff35569207515082803d70
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:41:32 +0400

    2.14  размер пустого массива n, заполненный в main

---
+++ Practice2.Task14/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Practice2.Task14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите размер массива:");
+        var inputDim=Console.ReadLine();
+        if (int.TryParse(inputDim, out var dim) && dim>0)
+        {
+            var array=CreateArray(dim);
+            for (int i = 0; i < dim; i++)
+            {
+                Console.WriteLine($"Введите {i} элемент массива:");
+                array[i] = Console.ReadLine();
+            }
+            PrintArray(array);
+        }
+        else
+        {
+            Console.WriteLine("Неверный формат входных данных.");
+        }
+    }
+
+    /// <summary>
+    /// Создать пустой массив.
+    /// </summary>
+    /// <param name="n">Размер массива.</param>
+    /// <returns>Созданный массив с заданной размерностью.</returns>
+    static string[] CreateArray(int n)
+    {
+        return new string[n];
+    }
+
+    /// <summary>
+    /// Вывести массив на экран.
+    /// </summary>
+    /// <param name="array">Массив.</param>
+    static void PrintArray(string[] array)
+    {
+        var row = string.Empty;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            row += array[i] + " ";
+        }
+        Console.WriteLine(row);
+    }
+}
