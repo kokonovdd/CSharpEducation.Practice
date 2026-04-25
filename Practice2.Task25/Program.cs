1246a814d40e816a56de32a404e380b7eefeaa12
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:48:13 +0400

    2.25 вывести из enum по числу.

---
+++ Practice2.Task25/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Practice2.Task25;
+
+class Program
+{
+    /// <summary>
+    /// Перечисление дней недели. 
+    /// </summary>
+    public enum DayOfWeek
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число от 1 до 7:");
+        var inputNumber=Console.ReadLine();
+
+        if (int.TryParse(inputNumber,out int number) && number > 0 && number < 7) 
+        {
+            Console.WriteLine(Enum.GetName(typeof(DayOfWeek), number));
+        }
+        else
+        {
+            Console.WriteLine("Неверный формат входных данных.");
+        }
+
+        Console.ReadLine();
+    }
+}
