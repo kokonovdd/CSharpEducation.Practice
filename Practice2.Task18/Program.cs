80b922e20de885236b6e128701189dd93243928d
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:44:02 +0400

    2.18 Массив без отрицательных чисел.

---
+++ Practice2.Task18/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Practice2.Task18;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, -3, -4, 5, };
+        Console.WriteLine("Массив на входе:");
+        PrintArray(array);
+
+        Console.WriteLine("Массив без отрицательных чисел:");
+        var newArray = ArrayWithoutNegativeValues(array, out int countNegativeValues);
+        PrintArray(newArray);
+
+        Console.WriteLine($"Количество удаленных чисел: {countNegativeValues}");
+
+        Console.ReadLine();
+    }
+
+    /// <summary>
+    /// Вывести массив на экран.
+    /// </summary>
+    /// <param name="array">Массив.</param>
+    static void PrintArray(int[] array)
+    {
+        var row = string.Empty;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            row += array[i] + " ";
+        }
+        Console.WriteLine(row);
+    }
+
+    /// <summary>
+    /// Получить массив без отрицательных чисел.
+    /// </summary>
+    /// <param name="array">Изначальный массив.</param>
+    /// <returns>Массив без отрицательных чисел.</returns>
+    static int[] ArrayWithoutNegativeValues(int[] array, out int countDeleted)
+    {
+        countDeleted = array.Count(e => e < 0);
+        return array.Where(e=>e>=0).ToArray();
+    }
+}
