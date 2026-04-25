e28715d9bf451eb5858465729dac5e8083e5a810
kokonovdd <denis.kokonov@gmail.com>
2026-04-25 15:47:10 +0400

    2.23 Високосный год.

---
+++ Practice2.Task23/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Practice2.Task23;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите год:");
+        var inputYear=Console.ReadLine();
+        bool isLeapYear = false;
+
+        if (int.TryParse(inputYear, out var year))
+        {
+            if (year % 400 == 0 || (!(year % 100 == 0) && (year % 4 == 0)))
+            {
+                isLeapYear = true;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неверный формат входных данных.");
+        }
+
+        if (isLeapYear) 
+        {
+            Console.WriteLine($"Год {year} - високосный.");
+        }
+        else
+        {
+            Console.WriteLine($"Год {year} - невисокосный.");
+        }
+
+        Console.ReadLine();
+    }
+}
